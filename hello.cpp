30b156a6faea21b021c6578b2f19a9ad8588dba8
taishi2020 <live.in.idleness8@gmail.com>
2021-02-19 16:19:35 +0900

    first commit

---
+++ hello.cpp
@@ -0,0 +1,28 @@
+/** --------------------------------------------------
+ * prototype declaration（プロトタイプ宣言）
+ */
+//#include <stdio.h>
+#include <iostream>
+//#include <vector>
+//#include "stdafx.h"
+//#include<string>
+//using namespace std;
+//#include<io.h>
+
+/* --------------------------------------------------
+ * return_various_sentence
+ * Parameters
+ * ----------
+ * none : void
+ * 
+ * Returns
+ * -------
+ * judge : int
+ */
+int main(void)
+{
+//	printf("hello! taishi!!＼(^o^)／\n");
+	std::cout << "hello! taishi!!＼(^o^)／" << std::endl;
+	std::cout << "hello! taishi!!＼(^o^)／" << std::endl;
+	return 0;
+}
